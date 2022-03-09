9e05b37d308b46257df3f0c0e53ec58885636f33
Declan <dh@38co.io>
2022-03-09 12:06:28 +0000

    added global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import CookieConsent from 'react-cookie-consent';
+import GlobalStyle from '../styles/GlobalStyles';
+import { MenuProvider } from '../Context/MenuContext';
+import Footer from '../Components/Footer/Footer';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MenuProvider>
+      <GlobalStyle />
+      <Component {...pageProps} />
+      <CookieConsent
+        location="top"
+        buttonText="Im cool with that"
+        cookieName="Alice and the Hair Cookie Consent"
+        style={{ background: '', boxShadow: '' }}
+        buttonStyle={{
+          background: '',
+          color: '',
+          fontSize: '14px',
+        }}
+        expires={150}
+      >
+        By using this website you agree to the use of cookies to enhance the
+        user experience.
+      </CookieConsent>
+    </MenuProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
