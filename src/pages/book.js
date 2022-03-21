a78e3e7f8564bae5780b4c2619cf77325af36617
Declan <dh@38co.io>
2022-03-21 19:48:58 +0000

    forced scaleable false

--- src/pages/book.js
+++ src/pages/book.js
@@ -1,12 +1,22 @@
+import Head from 'next/head';
+
 export default function Book() {
   return (
-    <div className="booking">
-      <iframe
-        title="booking page"
-        className="booking-page"
-        src="https://bookings.saloniq.co.uk/widget/dist/?id=ba4ec2c4-ae5f-4f81-a6de-82ee2a837ac8"
-        frameBorder="0"
-      />
-    </div>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </Head>
+      <div className="booking">
+        <iframe
+          title="booking page"
+          className="booking-page"
+          src="https://bookings.saloniq.co.uk/widget/dist/?id=ba4ec2c4-ae5f-4f81-a6de-82ee2a837ac8"
+          frameBorder="0"
+        />
+      </div>
+    </>
   );
 }
