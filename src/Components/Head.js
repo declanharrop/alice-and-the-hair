0bcd6cdfc8fe0d6e142e69ccb9cf32b152a91a02
Declan <dh@38co.io>
2022-04-29 16:22:24 +0100

    Added metadata

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -25,10 +25,10 @@ const Head = (props) => (
     <meta httpEquiv="expires" content="0" />
     <meta httpEquiv="pragma" content="no-cache" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" sizes="192x192" href="/static/favicon.ico" />
+    <link rel="icon" sizes="192x192" href="/favicon.ico" />
     <link rel="apple-touch-icon" href="/static/favicon.ico" />
     <link rel="mask-icon" href="/static/favicon.ico" color="#49B882" />
-    <link rel="icon" href="/static/favicon.ico" />
+    <link rel="icon" href="/favicon.ico" />
     <meta property="og:url" content={props.url || defaultOGURL} />
     <meta property="og:title" content={props.title || ''} />
     <meta
