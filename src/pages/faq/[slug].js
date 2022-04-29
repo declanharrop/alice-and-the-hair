62a154fb393079e12ef4c14b34fed69b9f70fb88
Declan <dh@38co.io>
2022-04-29 16:18:22 +0100

    Added metadata

--- src/pages/faq/[slug].js
+++ src/pages/faq/[slug].js
@@ -26,66 +26,64 @@ export default function FaqItem() {
   };
 
   return (
-    <>
-      <Head
-        title={data.section}
-        url={`https://aliceandthehair.co.uk/${slug}`}
-        description={data.metaDesc}
-      />
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-        <FaqItemStyles>
-          {slug && (
-            <>
-              <div className="uppersection slide-in-top">
-                <img src={`/img/faq/${data.slug}.jpg`} alt="" />
-                <div className="overlay">
-                  <h2>{data.section}</h2>
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+      <FaqItemStyles>
+        {slug && (
+          <>
+            <Head
+              title={data.section}
+              url={`https://aliceandthehair.co.uk/${slug}`}
+              description={data.metaDesc}
+            />
+            <div className="uppersection slide-in-top">
+              <img src={`/img/faq/${data.slug}.jpg`} alt="" />
+              <div className="overlay">
+                <h2>{data.section}</h2>
+              </div>
+            </div>
+            <div className="pageWrapper slide-in-bottom">
+              {data.introText && (
+                <div className="intro-text">
+                  {data.introText.map((text, a) => (
+                    <p key={a}>{text}</p>
+                  ))}
                 </div>
-              </div>
-              <div className="pageWrapper slide-in-bottom">
-                {data.introText && (
-                  <div className="intro-text">
-                    {data.introText.map((text, a) => (
-                      <p key={a}>{text}</p>
-                    ))}
+              )}
+              {data.content.map((item, i) => (
+                <div className="accordian" key={i}>
+                  <div
+                    className="q"
+                    onClick={() => toggleQuestion(item.q)}
+                    role="button"
+                  >
+                    <h4>{item.q}</h4>
                   </div>
-                )}
-                {data.content.map((item, i) => (
-                  <div className="accordian" key={i}>
-                    <div
-                      className="q"
-                      onClick={() => toggleQuestion(item.q)}
-                      role="button"
-                    >
-                      <h4>{item.q}</h4>
-                    </div>
-                    <AnimatePresence>
-                      {item.q === active && (
-                        <motion.div
-                          initial={animateFrom}
-                          animate={animateTo}
-                          exit={animateExit}
-                          transition={transition}
-                          className="a"
-                        >
-                          {item.a.map((text, t) => (
-                            <p key={t}>{text}</p>
-                          ))}
-                          {item.link && (
-                            <p>
-                              <a href={item.link}>{item.linkText}</a>
-                            </p>
-                          )}
-                        </motion.div>
-                      )}
-                    </AnimatePresence>
-                  </div>
-                ))}
-              </div>
-            </>
-          )}
-        </FaqItemStyles>
-      </motion.div>
-    </>
+                  <AnimatePresence>
+                    {item.q === active && (
+                      <motion.div
+                        initial={animateFrom}
+                        animate={animateTo}
+                        exit={animateExit}
+                        transition={transition}
+                        className="a"
+                      >
+                        {item.a.map((text, t) => (
+                          <p key={t}>{text}</p>
+                        ))}
+                        {item.link && (
+                          <p>
+                            <a href={item.link}>{item.linkText}</a>
+                          </p>
+                        )}
+                      </motion.div>
+                    )}
+                  </AnimatePresence>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+      </FaqItemStyles>
+    </motion.div>
   );
 }
