46c2ce57a973137d6cf8e4403e343aa0f60ca823
Declan <dh@38co.io>
2022-05-25 13:32:43 +0100

    Added mailchimp

---
+++ public/mailchimp.js
@@ -0,0 +1,11 @@
+!(function (c, h, i, m, p) {
+  (m = c.createElement(h)),
+    (p = c.getElementsByTagName(h)[0]),
+    (m.async = 1),
+    (m.src = i),
+    p.parentNode.insertBefore(m, p);
+})(
+  document,
+  'script',
+  'https://chimpstatic.com/mcjs-connected/js/users/fcb64d7808fba647fb81f5760/f80e34eca95eb3b89e1179c16.js'
+);
